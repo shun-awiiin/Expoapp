4dbae083cb6a2609e09a7f79edacfe09f051d679
awiiin <kikuchi_shun@awiiin.com>
2023-12-02 11:02:58 +0900

    追加処理

--- index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App'; // App.jsからAppコンポーネントをインポート
-import { name as appName } from './app.json'; // app.jsonからアプリ名を取得
-
-AppRegistry.registerComponent(appName, () => App);
